a58cd13f97031c8ffb7a0db76c8c5e61b933a38e
Chao Chen <chaochen234@gmail.com>
2023-03-01 15:24:37 -0500

    babel-preset & configs

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,16 @@ module.exports = {
         },
       },
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
         test: /\.s?css$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
