76d1edc54aaf71efc82c504cb011473e0e5f50ce
Chao Chen <chaochen234@gmail.com>
2023-03-02 18:45:05 -0500

    found scss issue

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,22 +1,22 @@
 import React from "react";
-import "/styles/AboutMe.css";
+import "/styles/AboutMe.scss";
 import { Email } from "react-obfuscate-email";
 
 const AboutMe: React.FC = () => {
   return (
-    <section className="about-me-section" id="about-me">
+    <div className="about-me-section">
       <h2 className="section__title">About me</h2>
-      <p id="about-me-desc">
+      <p className="about-me-desc">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam
       </p>
-      <div id="about-me-contacts">
+      <div className="about-me-contacts">
         <a target="_blank" href="test@email.com">
           <Email email="test@email.com">Contact Me</Email>
         </a>
       </div>
-    </section>
+    </div>
   );
 };
 
