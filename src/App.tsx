f8c194caaad6214a4049da7a0557a14284f6de2f
Chao Chen <chaochen234@gmail.com>
2023-03-21 18:14:15 -0400

    Removed comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// components
+import { intro, skills, experienceData, repos } from "./assets/Configurable.js";
 import AboutMe from "./components/AboutMe";
 import Navbar from "./components/Navbar";
 import Intro from "./components/Intro";
@@ -8,11 +8,6 @@ import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Footer from "./components/Footer";
 import Projects from "./components/Project";
-
-//import VerticalTab from "./components/Experience";
-import { intro, skills, experienceData, repos } from "./assets/Configurable.js";
-
-//assets
 
 const Home = React.forwardRef<any, any>((_, ref) => {
   return (
@@ -23,7 +18,6 @@ const Home = React.forwardRef<any, any>((_, ref) => {
         librariesFrameworks={skills.librariesFrameworks}
         otherSkills={skills.otherSkills}
       />
-      {/* <Experience experienceList={experience.data} /> */}
       <Experience data={experienceData.jobs} />
       <Projects
         username={repos.gitHubUsername}
@@ -38,8 +32,7 @@ const Home = React.forwardRef<any, any>((_, ref) => {
 const App = () => {
   const titleRef = React.useRef<HTMLDivElement>(null);
   return (
-    // <BrowserRouter basename={process.env.PUBLIC_URL + "/"}>
-    <BrowserRouter basename="">
+    <BrowserRouter basename={process.env.PUBLIC_URL + "/"}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home ref={titleRef} />} />
--- src/assets/Configurable.js
+++ src/assets/Configurable.js
@@ -107,38 +107,6 @@ const experienceData = {
     },
   ],
 };
-// const experienceData = {
-//   jobs: [
-//     {
-//       company: "Fake Company 1",
-//       position: "Software Engineer",
-//       period: "Oct 2018 - present",
-//       details: [
-//         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dignissim fringilla dui ac mattis.",
-//         "Donec in sodales eros. Nulla fermentum, ante in venenatis pellentesque, justo odio viverra lorem, varius posuere erat tortor et magna.",
-//       ],
-//     },
-//     {
-//       company: "Fake Company 2",
-//       position: "Software Developer",
-//       period: "Oct 2016 - May 2018",
-//       details: [
-//         "Suspendisse potenti. Vestibulum aliquam luctus sem, at feugiat felis. Pellentesque dignissim lorem eu ipsum condimentum varius. ",
-//         "Nam vehicula pretium arcu. Nam venenatis ante et porta pellentesque.",
-//       ],
-//     },
-//     {
-//       company: "Fake Company 3",
-//       position: "Engineer",
-//       period: "Mar 2016 - Oct 2016",
-//       details: [
-//         "Ut lobortis neque sit amet felis aliquam, sit amet placerat leo maximus. Aenean a tempus massa, vel varius leo.",
-//         "Sed eu efficitur leo, nec congue elit. Etiam ullamcorper est at pulvinar condimentum.",
-//         "Suspendisse potenti. Vestibulum aliquam luctus sem, at feugiat felis. Pellentesque dignissim lorem eu ipsum condimentum varius. ",
-//       ],
-//     },
-//   ],
-// };
 
 const repos = {
   gitHubUsername: "cchen26",
